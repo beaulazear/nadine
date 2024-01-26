fbd02874eb0b6ea8854a0189a6685a6c854a4a54
Beau <beaulazear@gmail.com>
2024-01-26 16:30:29 -0500

    changed size of offerings so they fit on mobile screens

--- src/components/Offering.js
+++ src/components/Offering.js
@@ -7,8 +7,8 @@ const StyledOfferingDiv = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 20px;
   margin-bottom: 20px;
-  width: 400px; /* Set a fixed width */
-  height: 250px; /* Set a fixed height */
+  width: 350px; /* Set a fixed width */
+  height: 275px; /* Set a fixed height */
   margin: 0 auto;
 `;
 
